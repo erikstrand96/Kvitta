dbbc626c918a67c415083eaa1fd624fcdfadc860
Erik Strand <erikstrand00@gmail.com>
2024-08-12 11:26:39 +0200

    - Get all valuables - Get valuable by id

--- Infrastructure/Database/Context/KvittaDbContextFactory.cs
+++ Infrastructure/Database/Context/KvittaDbContextFactory.cs
@@ -9,14 +9,9 @@ internal class KvittaDbContextFactory : IDesignTimeDbContextFactory<KvittaDbCont
     
     public KvittaDbContext CreateDbContext(string[] args)
     {
-        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
-        string appsettingsFile = environment.Equals("Development") ? "appsettings.Development.json" : "appsettings.json";
-        
-        IConfiguration config = new ConfigurationBuilder().AddJsonFile(appsettingsFile).Build();
-        string connectionString = config.GetConnectionString("POSTGRES_CONNECTION") ??
-                              throw new InvalidOperationException("POSTGRES_CONNECTION is not set.");
-        
         var optionsBuilder = new DbContextOptionsBuilder<KvittaDbContext>();
+        
+        string connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION") ?? throw new InvalidOperationException("POSTGRES_CONNECTION is not set.");
         
         optionsBuilder.UseNpgsql(connectionString);
         
--- Kvitta/Program.cs
+++ Kvitta/Program.cs
@@ -1,4 +1,8 @@
+using Infrastructure.Database.Context;
 using Infrastructure.Database.Extensions;
+using Infrastructure.Database.Models;using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +15,7 @@ IServiceCollection services = builder.Services;
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 
-string connectionString = config.GetConnectionString("POSTGRES_CONNECTION") ??
+string connectionString = Environment.GetEnvironmentVariable("POSTGRES_CONNECTION") ??
                           throw new InvalidOperationException("POSTGRES_CONNECTION is not set.");
 
 services.AddKvittaDbContext(connectionString);
@@ -27,5 +31,34 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/", string () => "Hello World!");
+
+app.MapPost("/valuables", async Task<IResult> (KvittaDbContext dbContext, Valuable valuable) =>
+{
+    dbContext.Valuables.Add(valuable);
+    await dbContext.SaveChangesAsync();
+
+    return Results.Created($"/valuables/{valuable.Id}", valuable);
+});
+
+app.MapGet("/valuables/{id}", async (Guid id, KvittaDbContext dbContext) =>
+{
+    Valuable? valuable = await dbContext.Valuables.FirstOrDefaultAsync(x => x.Id == id);
+
+    if (valuable is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(valuable);
+});
+
+app.MapGet("/valuables", async (KvittaDbContext dbContext) =>
+{
+    List<Valuable> valuables = await dbContext.Valuables.ToListAsync();
+
+    return Results.Ok(valuables);
+});
 
 await app.RunAsync();
