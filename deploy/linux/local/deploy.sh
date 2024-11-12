4f256fc72c5146db73729dfefe2d0faed746099e
Erik Strand <erikstrand00@gmail.com>
2024-11-12 09:49:23 +0100

    Formatting deploy.sh

--- deploy/linux/local/deploy.sh
+++ deploy/linux/local/deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 #The app runs using a linux system service under /etc/systemd/system/kvitta.service 
+# https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-nginx?view=aspnetcore-8.0&tabs=linux-ubuntu#create-the-service-file
 
 echo "Stopping kvitta service"
 sudo systemctl stop kvitta.service
 
-echo "Deploying application"
-dotnet publish -c Release -o /home/apps/kvitta/ --os linux
+echo
 
+echo "Deploying application"
+echo
+dotnet publish -c Release --property:PublishDir=/home/apps/kvitta/ --os linux
+
+echo
 sudo systemctl start kvitta.service
 sudo systemctl status kvitta.service
